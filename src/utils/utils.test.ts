bd6256b2c7fd16573c6afeb3f7d247535cfab371
Alex Santos <alexsantosquispe@gmail.com>
2025-10-01 22:21:29 -0400

    unit test for utils updated

--- src/utils/utils.test.ts
+++ src/utils/utils.test.ts
@@ -3,9 +3,19 @@ import { getWinnerResult, isDraw } from './utils';
 import type { SquareValueTypes } from '../types';
 
 describe('utils', () => {
+  const data: SquareValueTypes[] = ['X', 'X', 'X', 'O', 'O', '', '', '', 'O'];
+  const drawData: SquareValueTypes[] = [
+    'X',
+    'O',
+    'X',
+    'O',
+    'X',
+    'X',
+    'O',
+    'X',
+    'O'
+  ];
   describe('getWinnerResult', () => {
-    const data: SquareValueTypes[] = ['X', 'X', 'X', 'O', 'O', '', '', '', 'O'];
-
     const resultExpected = { winner: 'X', combHighLight: [0, 1, 2] };
 
     it('should return the winner', () => {
@@ -14,25 +24,26 @@ describe('utils', () => {
       expect(result.winner).toEqual(resultExpected.winner);
       expect(result.combHighLight).toEqual(resultExpected.combHighLight);
     });
+
+    it('should return default values', () => {
+      const result = getWinnerResult(drawData);
+
+      expect(result.winner).toEqual(null);
+      expect(result.combHighLight).toEqual([]);
+    });
   });
 
   describe('isDraw', () => {
-    const data: SquareValueTypes[] = [
-      'X',
-      'O',
-      'X',
-      'O',
-      'X',
-      'O',
-      'X',
-      'O',
-      'X'
-    ];
-
     it('should return true if the game is draw', () => {
-      const result = isDraw(data);
+      const result = isDraw(drawData);
 
       expect(result).toBe(true);
+    });
+
+    it('should return false if the game is not draw', () => {
+      const result = isDraw(data);
+
+      expect(result).toBe(false);
     });
   });
 });
