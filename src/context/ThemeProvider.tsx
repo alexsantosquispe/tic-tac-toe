f3e74cfaa3ecfbd0f199accdf2fa976e6efe462f
Alex Santos <alexsantosquispe@gmail.com>
2025-10-02 23:59:50 -0400

    the provider update

--- src/context/ThemeProvider.tsx
+++ src/context/ThemeProvider.tsx
@@ -1,5 +1,10 @@
 import { useEffect, useState, type ReactNode } from 'react';
-import { applyTheme, getStoredTheme } from '../utils/theme.utils';
+import {
+  applyTheme,
+  getStoredTheme,
+  getSystemTheme,
+  setStoredTheme
+} from '../utils/theme.utils';
 import { THEME_TYPES, ThemeContext, type ThemeType } from './ThemeContext';
 
 interface ThemeProviderProps {
@@ -7,9 +12,18 @@ interface ThemeProviderProps {
 }
 
 const ThemeProvider = ({ children }: ThemeProviderProps) => {
+  const initialTheme: ThemeType = getStoredTheme() as ThemeType;
   const [currentTheme, setCurrentTheme] = useState<ThemeType>(
-    THEME_TYPES.SYSTEM
+    initialTheme || THEME_TYPES.SYSTEM
   );
+
+  useEffect(() => {
+    const theme =
+      currentTheme === THEME_TYPES.SYSTEM ? getSystemTheme() : currentTheme;
+
+    applyTheme(theme);
+    setStoredTheme(currentTheme);
+  }, [currentTheme]);
 
   useEffect(() => {
     const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
