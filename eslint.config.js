0631a3cac394f34a508da52b38f87275dd105b5b
Alex Santos <alexsantosquispe@gmail.com>
2025-10-02 22:08:48 -0400

    pre-commit and pre-push and lint-staged setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,15 @@
 import { defineConfig, globalIgnores } from 'eslint/config';
 
+import globals from 'globals';
 import js from '@eslint/js';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  {
+    ignores: ['node_modules/**', 'dist/**', 'coverage/**']
+  },
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
