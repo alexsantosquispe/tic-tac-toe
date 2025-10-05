a59a656dee668f3c84cf67626e5c8b2a1cc45a15
Alex Santos <alexsantosquispe@gmail.com>
2025-10-04 22:21:56 -0400

    ToggleThemButton updated

--- src/components/atoms/ToggleThemeButton/ToggleThemeButton.tsx
+++ src/components/atoms/ToggleThemeButton/ToggleThemeButton.tsx
@@ -1,7 +1,6 @@
 import cn from 'clsx';
-import type { ReactNode } from 'react';
 import { twMerge } from 'tailwind-merge';
-import { THEME_TYPES, type ThemeType } from '../../../context/ThemeContext';
+import { THEME_TYPES } from '../../../context/ThemeContext';
 import { useTheme } from '../../../hooks/useTheme';
 import { MoonIcon } from '../../../icons/MoonIcon';
 import { SunIcon } from '../../../icons/SunIcon';
@@ -13,47 +12,25 @@ const THEME_OPTIONS = [
   { id: THEME_TYPES.DARK, icon: <MoonIcon /> }
 ];
 
-interface ThemeOptionProps {
-  id: ThemeType;
-  icon: ReactNode;
-  isSelected: boolean;
-  onSelectOption: (theme: ThemeType) => void;
-}
-
-export const ThemeOption = ({
-  id,
-  icon,
-  isSelected = false,
-  onSelectOption
-}: ThemeOptionProps) => {
-  return (
-    <button
-      aria-label={`${id} button`}
-      className={twMerge(
-        'rounded-full border-neutral-200 p-0.5 text-neutral-600 hover:cursor-pointer dark:border-white/20',
-        cn({ 'border bg-rose-600 text-white': isSelected })
-      )}
-      onClick={() => onSelectOption(id)}
-    >
-      {icon}
-    </button>
-  );
-};
-
 export const ToggleThemeButton = () => {
   const { theme, setTheme } = useTheme();
 
   return (
-    <div className="flex items-center gap-1.5 rounded-full border border-neutral-200 p-1 dark:border-white/20">
-      {THEME_OPTIONS.map((option) => {
-        const isSelected = option.id === theme;
+    <div className="flex items-center gap-1 rounded-full border border-neutral-200 p-1 dark:border-white/20">
+      {THEME_OPTIONS.map(({ id, icon }) => {
+        const isSelected = id === theme;
         return (
-          <ThemeOption
-            key={option.id}
-            {...option}
-            isSelected={isSelected}
-            onSelectOption={setTheme}
-          />
+          <button
+            key={id}
+            aria-label={`${id} button`}
+            className={twMerge(
+              'flex h-7 w-7 items-center justify-center rounded-full text-neutral-600 hover:cursor-pointer',
+              cn({ 'bg-rose-600 text-white': isSelected })
+            )}
+            onClick={() => setTheme(id)}
+          >
+            {icon}
+          </button>
         );
       })}
     </div>
