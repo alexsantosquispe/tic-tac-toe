ec6d43e2d71aa18772b3d9659bd838eac466c2e2
Alex Santos <alexsantosquispe@gmail.com>
2025-10-06 18:57:31 -0400

    feat: Translations setup and implementation added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { lazy, Suspense } from 'react';
+import { Suspense, lazy } from 'react';
+
 import { Footer } from './components/atoms/Footer/Footer';
+import { ResetIcon } from './icons/ResetIcon';
 import { Title } from './components/atoms/Title/Title';
 import useGame from './hooks/useGame';
-import { ResetIcon } from './icons/ResetIcon';
+import { useTranslation } from 'react-i18next';
 
 const Navbar = lazy(() => import('./components/atoms/Navbar/Navbar'));
 const Board = lazy(() => import('./components/molecules/Board/Board'));
 const Button = lazy(() => import('./components/atoms/Button/Button'));
 
 function App() {
+  const { t } = useTranslation();
   const { winner, currentPlayer, resetGame, isDraw } = useGame();
+
   return (
     <>
       <div className="flex min-h-screen flex-col bg-white font-normal text-neutral-700 uppercase dark:bg-black dark:text-white">
@@ -20,15 +24,16 @@ function App() {
           <section className="flex w-full flex-col items-center justify-center gap-6 px-4 md:gap-10">
             <h1 className="flex flex-col gap-2 text-center">
               <Title />
-              <span className="text-lg md:text-2xl">
-                Let's play a classic game!
-              </span>
+              <span className="text-lg md:text-2xl">{t('gameMessage')}</span>
             </h1>
             <div className="flex flex-col items-center justify-between gap-4 md:gap-8">
               <p className="text-3xl md:text-4xl" aria-live="polite">
-                {!winner && !isDraw && currentPlayer && `${currentPlayer} turn`}
-                {winner && `${winner} wins!`}
-                {isDraw && !winner && 'Draw!'}
+                {!winner &&
+                  !isDraw &&
+                  currentPlayer &&
+                  t('turn', { player: currentPlayer })}
+                {winner && t('win', { player: winner })}
+                {isDraw && !winner && t('draw')}
               </p>
               <Suspense
                 fallback={
@@ -39,8 +44,8 @@ function App() {
               </Suspense>
               <Suspense fallback={<div className="h-[3.75rem] w-[10rem]" />}>
                 <Button
-                  title="Reset"
-                  ariaLabel="Reset game button"
+                  title={t('resetButton.title')}
+                  ariaLabel={t('resetButton.ariaLabel')}
                   icon={<ResetIcon />}
                   onClick={resetGame}
                 />
