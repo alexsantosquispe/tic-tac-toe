78a9d67a068b62d5d60386f2ee1430a3f7a18049
Alex Santos <alexsantosquispe@gmail.com>
2025-10-06 23:36:46 -0400

    i18n debug false

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,8 +1,8 @@
+import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
 import en from './locales/en/translation.json';
 import es from './locales/es/translation.json';
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
 
 i18n
   .use(LanguageDetector)
@@ -13,7 +13,7 @@ i18n
       es: { translation: es }
     },
     fallbackLng: 'en',
-    debug: true,
+    debug: false,
     interpolation: {
       escapeValue: false
     },
