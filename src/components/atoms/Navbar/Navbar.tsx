1e9e88f421bc3e92864980f35c0329e29dbd006b
Alex Santos <alexsantosquispe@gmail.com>
2025-10-08 11:35:04 -0400

    Navbar and Tiutle comoponents updated

--- src/components/atoms/Navbar/Navbar.tsx
+++ src/components/atoms/Navbar/Navbar.tsx
@@ -1,11 +1,13 @@
 import { LanguageSwitcher } from '../LanguageSwitcher/LanguageSwitcher';
 import { ThemeSwitcher } from '../ThemeSwitcher/ThemeSwitcher';
-import { Title } from '../Title/Title';
 
 const Navbar = () => {
   return (
     <nav className="fixed z-50 flex h-14 w-full items-center justify-between border-b border-neutral-200 bg-white/50 px-4 font-semibold shadow backdrop-blur dark:border-white/20 dark:bg-black/60">
-      <Title className="text-lg md:text-xl" />
+      <p className="text-4xl text-black dark:text-white">
+        <span className="text-rose-600">X</span>O
+      </p>
+
       <div className="flex gap-2">
         <ThemeSwitcher />
         <LanguageSwitcher />
