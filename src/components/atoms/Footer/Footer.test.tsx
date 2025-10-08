9a8a2cf15039f6ba9879df3e20e318b28ee673fd
Alex Santos <alexsantosquispe@gmail.com>
2025-10-07 23:53:09 -0400

    github link added to the footer component

--- src/components/atoms/Footer/Footer.test.tsx
+++ src/components/atoms/Footer/Footer.test.tsx
@@ -1,14 +1,9 @@
-import { Footer } from './Footer';
-import { I18NWrapper } from '../../../tests/testsUtils';
 import { render } from '@testing-library/react';
+import { Footer } from './Footer';
 
 describe('Footer', () => {
   it('should render the component correctly', () => {
-    const component = render(
-      <I18NWrapper>
-        <Footer />
-      </I18NWrapper>
-    );
+    const component = render(<Footer />);
 
     expect(component).toMatchSnapshot();
   });
