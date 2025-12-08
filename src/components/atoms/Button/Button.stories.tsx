1103896cd83e7dc00ac62c5dcd524f80cf357961
Alex Santos <alexsantosquispe@gmail.com>
2025-12-07 22:17:58 -0400

    feat: Storybook added for Button component

---
+++ src/components/atoms/Button/Button.stories.tsx
@@ -0,0 +1,45 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { ItemWrapper, StoryContainer } from '../../../utils/StoriesWrapper';
+import Button from './Button';
+
+const meta = {
+  title: 'Atoms/Button',
+  component: Button,
+  parameters: {
+    layout: 'centered'
+  }
+} satisfies Meta<typeof Button>;
+
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const Default: Story = () => {
+  return (
+    <StoryContainer>
+      <ItemWrapper title="DEFAULT">
+        <Button
+          title="Button"
+          ariaLabel="button component"
+          onClick={() => {}}
+        />
+      </ItemWrapper>
+      <ItemWrapper title="DISABLED">
+        <Button
+          title="Button"
+          ariaLabel="button component"
+          onClick={() => {}}
+          isDisabled={true}
+        />
+      </ItemWrapper>
+    </StoryContainer>
+  );
+};
+
+Default.args = {
+  title: 'Button',
+  ariaLabel: 'button component',
+  onClick: () => {},
+  isDisabled: false,
+  className: ''
+};
